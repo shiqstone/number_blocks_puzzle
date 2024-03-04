4d1f22d2e318a005505fc404849c70a9b823e6eb
shiqstone <shiqstone@gmail.com>
2024-03-05 01:20:58 +0800

    add base widget

---
+++ lib/widgets/game/material/victory.dart
@@ -0,0 +1,90 @@
+import 'package:number_blocks_puzzle/data/result.dart';
+import 'package:number_blocks_puzzle/utils/time_format.dart';
+import 'package:flutter/material.dart';
+import 'package:share/share.dart';
+
+class GameVictoryDialog extends StatelessWidget {
+  final Result result;
+
+  final String Function(int) timeFormatter;
+
+  GameVictoryDialog({
+    required this.result,
+    this.timeFormatter = formatElapsedTime,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final timeFormatted = timeFormatter(result.time);
+    final actions = <Widget>[
+      new TextButton(
+        child: new Text("Share"),
+        onPressed: () {
+          Share.share("I have solved the Number Block Puzzle's "
+              "${result.size}x${result.size} puzzle in $timeFormatted "
+              "with just ${result.steps} steps! ");
+        },
+      ),
+      new TextButton(
+        child: new Text("Close"),
+        onPressed: () {
+          Navigator.of(context).pop();
+        },
+      ),
+    ];
+
+    return AlertDialog(
+      title: Center(
+        child: Text(
+          "Congratulations!",
+          style: Theme.of(context).textTheme.headlineSmall,
+        ),
+      ),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Text("You've successfuly completed the ${result.size}x${result.size} puzzle"),
+          const SizedBox(height: 16),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Text(
+                    'Time:',
+                    style: Theme.of(context).textTheme.bodySmall,
+                  ),
+                  Text(
+                    timeFormatted,
+                    style: Theme.of(context).textTheme.displaySmall!.copyWith(
+                          color: Theme.of(context).textTheme.bodyLarge!.color,
+                        ),
+                  ),
+                ],
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Text(
+                    'Steps:',
+                    style: Theme.of(context).textTheme.bodySmall,
+                  ),
+                  Text(
+                    '${result.steps}',
+                    style: Theme.of(context).textTheme.displaySmall!.copyWith(
+                          color: Theme.of(context).textTheme.bodyLarge!.color,
+                        ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+      actions: actions,
+    );
+  }
+}
