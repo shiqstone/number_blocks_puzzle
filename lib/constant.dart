e8702160a904e8f86d060a0bf4db883edc727c27
shiqstone <shiqstone@gmail.com>
2024-03-06 00:46:58 +0800

    add constant config

---
+++ lib/constant.dart
@@ -0,0 +1,9 @@
+
+class Constant {
+
+  static const String mateKeyStr = 'df60bc65c63c5913';
+  static const String mateIvStr  = 'e3e3e254';
+
+  static const URL_REPOSITORY = "https://github.com/shiqstone/number_blocks_puzzle";
+  static const URL_FEEDBACK = "https://github.com/shiqstone/number_blocks_puzzle/issues";
+}
